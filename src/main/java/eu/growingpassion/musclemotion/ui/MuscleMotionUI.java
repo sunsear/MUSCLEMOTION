34b6d040daa8a3e24d4d6086af89cc4162cd7f28
Martin van Dijken <martin.van.dijken@rigd-loxia.nl>
2018-02-07 10:48:28 +0100

    Simpler plugin implementation

--- src/main/java/eu/growingpassion/musclemotion/ui/MuscleMotionUI.java
+++ src/main/java/eu/growingpassion/musclemotion/ui/MuscleMotionUI.java
@@ -1,19 +1,13 @@
 package eu.growingpassion.musclemotion.ui;
 
 import ij.IJ;
-import ij.ImagePlus;
-import ij.plugin.filter.PlugInFilter;
-import ij.process.ImageProcessor;
+import ij.plugin.PlugIn;
 
-public class MuscleMotionUI implements PlugInFilter {
-    public int setup(java.lang.String s, ImagePlus imagePlus) {
+public class MuscleMotionUI implements PlugIn {
+
+    public void run(String args) {
         IJ.showMessage("MuscleMotion",
                 "measuring heart muscle cell contractions"
         );
-        return DOES_STACKS | STACK_REQUIRED;
-    }
-
-    public void run(ImageProcessor imageProcessor) {
-        IJ.showMessage("Hi!");
     }
 }
